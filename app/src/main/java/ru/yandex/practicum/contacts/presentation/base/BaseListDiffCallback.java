77a1ef1caf7c04b16a168255f9ac9b698a4a9ff0
Antonova MArgarita <rita@rita321.ru>
2024-02-06 01:57:48 +0300

    realised step 3

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -9,18 +9,18 @@ import ru.yandex.practicum.contacts.presentation.main.ContactUi;
 
 public class BaseListDiffCallback <S extends ListDiffInterface<S>> extends DiffUtil.ItemCallback<S>{
     @Override
-    public boolean areItemsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public boolean areItemsTheSame(@NonNull S oldItem, @NonNull S newItem) {
         return oldItem.hashCode() == newItem.hashCode();
     }
 
     @Override
-    public boolean areContentsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public boolean areContentsTheSame(@NonNull S oldItem, @NonNull S newItem) {
         return oldItem.equals(newItem);
     }
 
     @Nullable
     @Override
-    public Object getChangePayload(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public Object getChangePayload(@NonNull S oldItem, @NonNull S newItem) {
         return newItem;
     }
 }
